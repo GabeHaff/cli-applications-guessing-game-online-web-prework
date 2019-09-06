a003709b6d70510c4a638496e40de2e4dfbea8c1
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 14:03:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,10 @@ def run_guessing_game
   random =rand(1...7)
   puts "guess a number between 1 and 6."
   user_input = gets.chomp 
-if user_input=="exit"
+if user_input=random
+elsif user_input=="exit"
     puts "Goodbye!"
-  elsif user_input == random
-    puts "you guessed the correct number!"
+
   else
     puts "Sorry, the computer guessed #{random}."
    
