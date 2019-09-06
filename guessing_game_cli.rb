5c10a617155892bff77e622f8282124332d2dd7b
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 13:36:37 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,8 @@ def run_guessing_game
   user_input = gets.chomp 
   if user_input == random
     puts "you guessed the correct number"
-    
+     if user_input !=random
+      puts "Sorry, the computer guessed #{random}"
   elsif user_input=="exit"
     puts "Goodbye!"
  
