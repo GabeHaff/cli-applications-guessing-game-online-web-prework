09c014de1861c31b6bf8a214ffea62d480c28698
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-06 13:46:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   puts "guess a number between 1 and 6"
   user_input = gets.chomp 
   if random == user_input
-    puts "you guessed the correct number"
+    return "you guessed the correct number"
   elsif user_input=="exit"
     puts "Goodbye!"
  else
